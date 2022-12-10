2c93540cbb80997c3187674137d239cf57030e1f
Arda <sariarda5858@gmail.com>
2022-12-10 16:46:44 +0300

    update

--- src/renderer/components/explore/Hero.vue
+++ src/renderer/components/explore/Hero.vue
@@ -3,40 +3,42 @@
     <h1>{{ title }}</h1>
     <p>{{ subtitle }}</p>
     <div class="hero__input">
-      <input
-        v-model:value="Search"
-        type="text"
-        name="input"
-        :placeholder="$props.input"
-        @focus="onFocus"
-        @blur="onBlur"
-      />
-      <button
-        v-if="this.Search.length > 0"
-        class="hero__input-clear"
-        v-on:click="clear"
-      >
-        <svg
-          class="clearIcon-TMIIUa closeIcon-BMGxx0"
-          aria-hidden="true"
-          role="img"
-          width="24"
-          height="24"
-          viewBox="0 0 14 14"
+      <div class="hero__input-search">
+        <input
+          v-model:value="Search"
+          type="text"
+          name="input"
+          :placeholder="$props.input"
+          @focus="onFocus"
+          @blur="onBlur"
+        />
+        <button
+          v-if="this.Search.length > 0"
+          class="hero__input-search-clear"
+          v-on:click="clear"
         >
-          <path
-            fill="currentColor"
-            d="M7.02799 0.333252C3.346 0.333252 0.361328 3.31792 0.361328 6.99992C0.361328 10.6819 3.346 13.6666 7.02799 13.6666C10.71 13.6666 13.6947 10.6819 13.6947 6.99992C13.6947 3.31792 10.7093 0.333252 7.02799 0.333252ZM10.166 9.19525L9.22333 10.1379L7.02799 7.94325L4.83266 10.1379L3.89 9.19525L6.08466 6.99992L3.88933 4.80459L4.832 3.86259L7.02733 6.05792L9.22266 3.86259L10.1653 4.80459L7.97066 6.99992L10.166 9.19525Z"
-          ></path>
-        </svg>
-      </button>
-      <p
-        class="hero__input-info"
-        v-if="this.Search.length > 1"
-        :style="{ opacity: inputBackground }"
-      >
-        "ENTER" to Search
-      </p>
+          <svg
+            class="clearIcon-TMIIUa closeIcon-BMGxx0"
+            aria-hidden="true"
+            role="img"
+            width="24"
+            height="24"
+            viewBox="0 0 14 14"
+          >
+            <path
+              fill="currentColor"
+              d="M7.02799 0.333252C3.346 0.333252 0.361328 3.31792 0.361328 6.99992C0.361328 10.6819 3.346 13.6666 7.02799 13.6666C10.71 13.6666 13.6947 10.6819 13.6947 6.99992C13.6947 3.31792 10.7093 0.333252 7.02799 0.333252ZM10.166 9.19525L9.22333 10.1379L7.02799 7.94325L4.83266 10.1379L3.89 9.19525L6.08466 6.99992L3.88933 4.80459L4.832 3.86259L7.02733 6.05792L9.22266 3.86259L10.1653 4.80459L7.97066 6.99992L10.166 9.19525Z"
+            ></path>
+          </svg>
+        </button>
+        <p
+          class="hero__input-search-info"
+          v-if="this.Search.length > 1"
+          :style="{ display: inputBackground }"
+        >
+          "ENTER" to Search
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -53,10 +55,10 @@ export default {
   },
   methods: {
     onFocus() {
-      this.inputBackground = "1";
+      this.inputBackground = "flex";
     },
     onBlur() {
-      this.inputBackground = "0";
+      this.inputBackground = "none";
     },
     clear() {
       this.Search = "";
@@ -67,7 +69,6 @@ export default {
 
 <style lang="scss" scoped>
 .hero {
-  width: 100%;
   aspect-ratio: 3.20331950207;
   margin-top: 32px;
   border-radius: 8px;
@@ -78,6 +79,8 @@ export default {
   justify-content: center;
   flex-direction: column;
   gap: 12px;
+  width: 100%;
+
   * {
     margin: 0;
     color: #fff;
@@ -93,13 +96,20 @@ export default {
     line-height: 24px;
   }
   &__input {
+    min-width: 360px;
+    width: 50%;
+    max-width: 600px;
+  }
+  &__input-search {
     position: relative;
     margin-top: 4px;
     display: flex;
     align-items: center;
+    justify-content: center;
+    width: 100%;
     input {
+      width: 100%;
       color: #202225;
-      width: 550px;
       border-radius: 4px;
       border: none;
       font-size: 14px;
@@ -141,7 +151,6 @@ export default {
       right: 42px;
       font-size: 12px !important;
       color: #4f545c;
-      opacity: 0;
     }
   }
 }
